7098023f3c840c9f18660492921fac66bbe25ff4
Quentin Gabriele <quentin.gabriele@gmail.com>
2025-08-14 20:58:15 +0200

    fix

--- src/Forms/Components/ElegantlyMediaFileUpload.php
+++ src/Forms/Components/ElegantlyMediaFileUpload.php
@@ -163,6 +163,13 @@ class ElegantlyMediaFileUpload extends FileUpload
         return $this;
     }
 
+    public function group(string|Closure|null $group): static
+    {
+        $this->group = $group;
+
+        return $this;
+    }
+
     public function conversion(string|Closure|null $conversion): static
     {
         $this->conversion = $conversion;
